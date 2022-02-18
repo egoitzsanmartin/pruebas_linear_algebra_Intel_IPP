67e116856e2690f03152eddb0d6a3744bdbe6c06
Anesmi <egoitz.sanmartin@alumni.mondragon.edu>
2022-02-18 13:24:27 +0100

    V0.0

---
+++ la_sample/PENTIUM4gnu/la_sample/Debug/Objects/la_src/main.d
@@ -0,0 +1,215 @@
+la_sample/Debug/Objects/la_src/main.o:  \
+ C:/WindRiver/workspace/la_src/main.c \
+  C:/WindRiver/vxworks-6.9/target/h/stdio.h \
+  C:/WindRiver/vxworks-6.9/target/h/types/vxANSI.h \
+  C:/WindRiver/vxworks-6.9/target/h/stdint.h \
+  C:/WindRiver/vxworks-6.9/target/h/vsbConfig.h \
+  C:/WindRiver/vxworks-6.9/target/lib/h/config/vsbConfig.h \
+  C:/WindRiver/vxworks-6.9/target/lib/h/config/autoconf.h \
+  C:/WindRiver/vxworks-6.9/target/h/types/vxCpu.h \
+  C:/WindRiver/vxworks-6.9/target/h/types/vxArch.h \
+  C:/WindRiver/vxworks-6.9/target/h/arch/i86/archI86.h \
+  C:/WindRiver/vxworks-6.9/target/h/arch/i86/archI86Common.h \
+  C:/WindRiver/vxworks-6.9/target/h/types/vxParams.h \
+  C:/WindRiver/vxworks-6.9/target/h/types/vxTypesBase.h \
+  c:\windriver\gnu\4.3.3-vxworks-6.9\x86-win32\bin\../../lib/gcc/i586-wrs-vxworks/4.3.3/include/stddef.h \
+  C:/WindRiver/vxworks-6.9/target/h/errno.h \
+  C:/WindRiver/vxworks-6.9/target/h/types/vxANSI.h \
+  C:/WindRiver/vxworks-6.9/target/h/private/handleLibP.h \
+  C:/WindRiver/vxworks-6.9/target/h/vxWorks.h \
+  C:/WindRiver/vxworks-6.9/target/h/vxWorksCommon.h \
+  C:/WindRiver/vxworks-6.9/target/h/types/vxTypes.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_ssize_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_cpuset_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_phys_cpuset_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_atomic_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_time_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_dev_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_gid_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_ino_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_mode_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_nlink_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_off_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_pid_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_uid_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_blksize_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_blkcnt_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/types/vxTypesOld.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/e_vxTypesOld.h \
+  C:/WindRiver/vxworks-6.9/target/h/sys/types.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_pthread_attr_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_size_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_struct__Sched_param.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_struct__Timespec.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_clock_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_key_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_timer_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_ptrdiff_t.h \
+  C:/WindRiver/vxworks-6.9/target/h/base/b_BOOL.h \
+  C:/WindRiver/vxworks-6.9/target/h/types/vxWind.h \
+  C:/WindRiver/vxworks-6.9/target/h/types/vxWindCommon.h \
+  C:/WindRiver/vxworks-6.9/target/h/tool/gnu/toolMacros.h \
+  C:/WindRiver/vxworks-6.9/target/h/tool/common/private/toolMacrosP.h \
+  C:/WindRiver/vxworks-6.9/target/h/version.h \
+  c:\windriver\gnu\4.3.3-vxworks-6.9\x86-win32\bin\../../lib/gcc/i586-wrs-vxworks/4.3.3/include/stdarg.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ipp.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippversion.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippdefs.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippcore.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippac.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippcc.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippdc.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippch.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippcv.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippi.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippj.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippm.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ipps.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippsc.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippvc.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippvm.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippr.h \
+  C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippdi.h \
+  C:/WindRiver/vxworks-6.9/target/h/math.h
+
+C:/WindRiver/vxworks-6.9/target/h/stdio.h:
+
+C:/WindRiver/vxworks-6.9/target/h/types/vxANSI.h:
+
+C:/WindRiver/vxworks-6.9/target/h/stdint.h:
+
+C:/WindRiver/vxworks-6.9/target/h/vsbConfig.h:
+
+C:/WindRiver/vxworks-6.9/target/lib/h/config/vsbConfig.h:
+
+C:/WindRiver/vxworks-6.9/target/lib/h/config/autoconf.h:
+
+C:/WindRiver/vxworks-6.9/target/h/types/vxCpu.h:
+
+C:/WindRiver/vxworks-6.9/target/h/types/vxArch.h:
+
+C:/WindRiver/vxworks-6.9/target/h/arch/i86/archI86.h:
+
+C:/WindRiver/vxworks-6.9/target/h/arch/i86/archI86Common.h:
+
+C:/WindRiver/vxworks-6.9/target/h/types/vxParams.h:
+
+C:/WindRiver/vxworks-6.9/target/h/types/vxTypesBase.h:
+
+c:\windriver\gnu\4.3.3-vxworks-6.9\x86-win32\bin\../../lib/gcc/i586-wrs-vxworks/4.3.3/include/stddef.h:
+
+C:/WindRiver/vxworks-6.9/target/h/errno.h:
+
+C:/WindRiver/vxworks-6.9/target/h/types/vxANSI.h:
+
+C:/WindRiver/vxworks-6.9/target/h/private/handleLibP.h:
+
+C:/WindRiver/vxworks-6.9/target/h/vxWorks.h:
+
+C:/WindRiver/vxworks-6.9/target/h/vxWorksCommon.h:
+
+C:/WindRiver/vxworks-6.9/target/h/types/vxTypes.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_ssize_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_cpuset_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_phys_cpuset_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_atomic_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_time_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_dev_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_gid_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_ino_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_mode_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_nlink_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_off_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_pid_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_uid_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_blksize_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_blkcnt_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/types/vxTypesOld.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/e_vxTypesOld.h:
+
+C:/WindRiver/vxworks-6.9/target/h/sys/types.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_pthread_attr_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_size_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_struct__Sched_param.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_struct__Timespec.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_clock_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_key_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_timer_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_ptrdiff_t.h:
+
+C:/WindRiver/vxworks-6.9/target/h/base/b_BOOL.h:
+
+C:/WindRiver/vxworks-6.9/target/h/types/vxWind.h:
+
+C:/WindRiver/vxworks-6.9/target/h/types/vxWindCommon.h:
+
+C:/WindRiver/vxworks-6.9/target/h/tool/gnu/toolMacros.h:
+
+C:/WindRiver/vxworks-6.9/target/h/tool/common/private/toolMacrosP.h:
+
+C:/WindRiver/vxworks-6.9/target/h/version.h:
+
+c:\windriver\gnu\4.3.3-vxworks-6.9\x86-win32\bin\../../lib/gcc/i586-wrs-vxworks/4.3.3/include/stdarg.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ipp.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippversion.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippdefs.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippcore.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippac.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippcc.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippdc.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippch.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippcv.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippi.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippj.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippm.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ipps.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippsc.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippvc.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippvm.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippr.h:
+
+C:/WindRiver/intel/20160311-vxworks-6.9/ipp/include/ippdi.h:
+
+C:/WindRiver/vxworks-6.9/target/h/math.h:
